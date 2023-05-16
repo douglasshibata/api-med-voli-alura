3837268639961cdb0f435daa1415efbce7eb964f
Douglas Shibata <douglas.shibata11@gmail.com>
2023-05-16 11:53:24 -0300

    feat: handler expection

--- src/main/java/alura/med/voli/api/domain/endereco/DadosEndereco.java
+++
@@ -1,6 +0,0 @@
-package alura.med.voli.api.domain.endereco;
-
-public record DadosEndereco(String logradouro, String bairro, String cep, String cidade, String uf, String complemento,
-        String numero) {
-
-}
--- src/main/java/alura/med/voli/api/domain/medico/Especialidade.java
+++
@@ -1,9 +0,0 @@
-package alura.med.voli.api.domain.medico;
-
-public enum Especialidade {
-
-    ORTOPEDIA,
-    CARDIOLOGIA,
-    GINECOLOGIA,
-    DEMARTOLOGIA,
-}
